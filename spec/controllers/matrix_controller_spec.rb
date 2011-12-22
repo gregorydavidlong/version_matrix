528c59888397ca2f0671f3bb024a9cf0d89dcde8
Gregory Long <gregorydavidlong@me.com>
2011-12-22 22:38:03 +1100

    Added tests for fetching the version matrix data

--- spec/controllers/matrix_controller_spec.rb
+++ spec/controllers/matrix_controller_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
-describe MatrixController do
+describe MatriciesController do
 
   describe "GET 'matrix'" do
     it "should be successful" do
       get 'matrix'
       response.should be_success
     end
+
+
+    it "should fetch the compatible versions"
   end
 
 end
